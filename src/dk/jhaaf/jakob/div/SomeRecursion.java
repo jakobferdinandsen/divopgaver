7d93cf30b5ac1e8a6dfe9559466eba6db2c681e4
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-03-13 23:52:54 +0100

    Added some output

--- src/dk/jhaaf/jakob/div/SomeRecursion.java
+++ src/dk/jhaaf/jakob/div/SomeRecursion.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
  * Created by jakob on 13-03-17.
  */
 public class SomeRecursion {
+    private int recurCount;
+
+
     public static void main(String[] args) {
+
         SomeRecursion recursion = new SomeRecursion();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter an integer");
@@ -14,11 +18,21 @@ public class SomeRecursion {
         System.out.println("Result: " + recursion.recurFunc(input));
     }
 
+
     public int recurFunc(int input) {
+        recurCount++;
         if (input <= 2) {
+            System.out.println(recurCount);
+            System.out.println(input);
+            System.out.println("Hit i <= 2");
+            System.out.println();
             return input;
         }
-        return 2 * recurFunc(input - 1) + recurFunc(input - 2) - recurFunc(input - 3);
+        System.out.println(recurCount);
+        System.out.println(input);
+        System.out.println("Hit recursion");
+        System.out.println();
+        return recurFunc(input - 1) + recurFunc(input - 2);
     }
 
 }
